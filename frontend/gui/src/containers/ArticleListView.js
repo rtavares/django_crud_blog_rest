495af5a3ca4d657884630bf097c4f232a223ac54
Raul Tavares <raul.tavares@limejump.com>
2019-02-11 00:31:37 +0000

    - Connecting front-end data to back-end API.

--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import Articles  from '../components/Article';
 
@@ -14,9 +15,24 @@ for (let i = 0; i < 23; i++) {
 }
 
 class ArticleList extends React.Component {
+
+    state = {
+        articles: []
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/')
+            .then(res => {
+                this.setState({
+                    articles: res.data
+                });
+                console.log(res.data);
+            });
+    };
+
     render() {
         return (
-            <Articles data={listData}/>
+            <Articles data={this.state.articles}/>
         )
     }
 }
